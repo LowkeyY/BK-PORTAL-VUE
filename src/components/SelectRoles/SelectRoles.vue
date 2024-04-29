970a6c9500844abaf486b61e74d835b052b25672
laiyonghong <laiyonghong@cunovs.com>
2024-04-29 16:43:10 +0800

    feat:选择角色com

---
+++ src/components/SelectRoles/SelectRoles.vue
@@ -0,0 +1,41 @@
+<template>
+    <uni-popup ref="popupRef" background-color="#fff">
+        <view class="popup-content">
+            <view class="action-item border" @click="handelSelect('bjou_student')">北开学生</view>
+            <view class="action-item border" @click="handelSelect('ouchn_student')">国开学生</view>
+            <view class="action-item cancel" @click="popupRef.close()">取消</view>
+        </view>
+    </uni-popup>
+</template>
+
+<script setup lang="ts" name="SelectRoles">
+const emit = defineEmits(['selectRole']);
+const popupRef = ref();
+
+const handelSelect = (role) => {
+    emit('selectRole', role);
+    popupRef.value.close();
+};
+const toggle = () => {
+    popupRef.value.open('bottom');
+};
+defineExpose({
+    toggle,
+});
+</script>
+
+<style scoped lang="scss">
+.popup-content {
+    .action-item {
+        font-size: $uni-font-size-lg;
+        color: $uni-color-title;
+        padding: 30rpx 0;
+    }
+    .border {
+        border-bottom: 1px solid $uni-border-color;
+    }
+    .cancel {
+        color: #ca3120;
+    }
+}
+</style>
